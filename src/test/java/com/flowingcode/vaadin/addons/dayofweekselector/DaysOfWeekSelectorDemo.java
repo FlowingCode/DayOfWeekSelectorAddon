ef6691c1eaf899d0df4a35d32530ce79bbefc4d0
Javier Godoy <11554739+javier-godoy@users.noreply.github.com>
2023-12-22 19:54:52 -0300

    feat(demo): add demo for read-only and disabled states

--- src/test/java/com/flowingcode/vaadin/addons/dayofweekselector/DaysOfWeekSelectorDemo.java
+++ src/test/java/com/flowingcode/vaadin/addons/dayofweekselector/DaysOfWeekSelectorDemo.java
@@ -4,14 +4,27 @@ import com.flowingcode.vaadin.addons.demo.DemoSource;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import java.time.DayOfWeek;
 
 @DemoSource
-@PageTitle("DaysOfWeekSelector Add-on Demo")
+@PageTitle("Demo")
 @SuppressWarnings("serial")
 @Route(value = "day-of-week-selector", layout = DaysOfWeekSelectorDemoView.class)
 public class DaysOfWeekSelectorDemo extends Div {
 
   public DaysOfWeekSelectorDemo() {
-    add(new DaysOfWeekSelector());
+    DaysOfWeekSelector selector1 =
+        new DaysOfWeekSelector("Enabled", DayOfWeek.MONDAY, DayOfWeek.WEDNESDAY);
+    add(selector1);
+
+    DaysOfWeekSelector selector2 =
+        new DaysOfWeekSelector("Read-only", DayOfWeek.MONDAY, DayOfWeek.WEDNESDAY);
+    selector2.setReadOnly(true);
+    add(selector2);
+
+    DaysOfWeekSelector selector3 =
+        new DaysOfWeekSelector("Disabled", DayOfWeek.MONDAY, DayOfWeek.WEDNESDAY);
+    selector3.setEnabled(false);
+    add(selector3);
   }
 }
